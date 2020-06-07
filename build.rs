faa25fe174f5ba338633169338bee1395b4bb20b
Moritz Moeller <virtualritz@gmail.com>
2020-06-07 17:14:05 +0200

    Initial commit to repo with removed lfs files.

---
+++ build.rs
@@ -0,0 +1,72 @@
+#[cfg(any(target_os = "windows", target_os = "macos", target_os = "linux"))]
+// build.rs
+extern crate bindgen;
+extern crate reqwest;
+
+use std::{
+    env,
+    io::Write,
+    path::{Path, PathBuf},
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // TODO: make this generic & work on Linux/Windows
+
+    let (include_path, lib_path) = match &env::var("DELIGHT") {
+        Err(_) => {
+            eprintln!("Building against 3Delight 2.1.2");
+
+            #[cfg(target_os = "windows")]
+            let lib = "https://www.dropbox.com/s/9iavkggor0ecc1x/3Delight.dll";
+            #[cfg(target_os = "macos")]
+            let lib = "https://www.dropbox.com/s/7vle92kcqbbyn8o/lib3delight.dylib";
+            #[cfg(target_os = "linux")]
+            let lib = "https://www.dropbox.com/s/hr62te8yg1d2e36/lib3delight.so";
+
+            let lib_path = PathBuf::from(&env::var("OUT_DIR")?);
+            let lib_file_path = lib_path.join(Path::new(lib).file_name().unwrap());
+
+            if !lib_file_path.exists() {
+                // Download the libs to build against.
+                let lib_data = reqwest::blocking::get(lib)?.bytes()?;
+                std::fs::File::create(lib_file_path)?.write_all(&lib_data)?;
+            }
+
+            (
+                PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap()).join("include"),
+                lib_path,
+            )
+        }
+        Ok(path) => {
+            eprintln!("Building against locally installed 3Delight @ {}", &path);
+            let delight = Path::new(&path);
+
+            (delight.join("include"), delight.join("lib"))
+        }
+    };
+
+    eprintln!("include: {}", include_path.display());
+    eprintln!("lib:     {}", lib_path.display());
+
+    // Emit linker searchpath
+    println!("cargo:rustc-link-search={}", lib_path.display());
+    // Link to lib3delight
+    println!("cargo:rustc-link-lib=dylib=3delight");
+
+    // Build bindings
+    let bindings = bindgen::Builder::default()
+        .header("include/wrapper.h")
+        // Searchpath
+        .clang_arg(format!("-I{}", include_path.display()))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings.");
+
+    Ok(())
+}
---
+++ include/wrapper.h
@@ -0,0 +1,1 @@
+#include <nsi.h>
